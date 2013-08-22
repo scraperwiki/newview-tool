975c64422f02420f7d29223240c11c5703914284
Peter Waller <peter@scraperwiki.com>
2013-08-22 15:20:54 +0100

    wipe-myself: Transition to symlinks

--- wipe-myself.sh
+++ wipe-myself.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -fr tool/.git
-rm tool/scraperwiki.json
-rm tool/wipe-myself.sh
-rm tool/README.md
-rm tool/LICENCE
+cp tool/http/index.html .
+rm tool http
+mkdir -p tool/http
+ln -s tool/http
+mv index.html tool/http
 
 sed -i 's/^.*wipe-myself.sh.*$//' tool/http/index.html
 
